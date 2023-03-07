551ae2fd9e3f326e8d0a617565890f49f2b89621
Fabrício de Figueiredo Miranda <125789205+fabriciofianda@users.noreply.github.com>
2023-03-07 07:25:03 -0300

    Add files via upload

---
+++ py.py
@@ -0,0 +1,47 @@
+loop = 1
+while loop == 1:
+    print("___________________________________________")
+    sacar = float(input("Digite o montante a sacar..."))
+    print("___________________________________________")
+    metodo = int(input("Digite o metodo [1] Pix [2] Maquininha de Cartao ...."))
+
+    if metodo == 1:
+        total = sacar + (sacar * 0.1)
+        print("Total a sacar: {}".format(total))
+        continuar = input("Digite [Y] para continuar ou [N] para sair...")
+        continuar = continuar.upper()
+        if continuar == 'Y':
+            continue
+        elif continuar == 'N':
+            loop = 0
+        else:
+            input("Caractere invalido * {} * nao declarado no escopo...")
+            continue
+    elif metodo == 2:
+        total = sacar + (sacar * 0.3)
+        print("Total a sacar: {}".format(total))
+        continuar = input("Digite [Y] para continuar ou [N] para sair...")
+        continuar = continuar.upper()
+        if continuar == 'Y':
+            continue
+        elif continuar == 'N':
+            loop = 0
+        else:
+            input("Caractere invalido * {} * nao declarado no escopo...")
+            continue
+    elif metodo == 3:
+        taxa = int(input("Digite a taxa para sacar..."))
+        total = sacar + (sacar * (taxa / 100))
+        print("Total a sacar: {}".format(total))
+        continuar = input("Digite [Y] para continuar ou [N] para sair...")
+        continuar = continuar.upper()
+        if continuar == 'Y':
+            continue
+        elif continuar == 'N':
+            loop = 0
+        else:
+            input("Caractere invalido * {} * nao declarado no escopo...")
+            continue
+    else:
+        print("Digite um metodo disponivel * {} * nao declarado no escopo".format(metodo))
+        continue
