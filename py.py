7b80e1fa233f5d915696748640ecaec65b024f96
Fabrício de Figueiredo Miranda <125789205+fabriciofianda@users.noreply.github.com>
2023-03-07 07:37:39 -0300

    Update py.py

--- py.py
+++ py.py
@@ -1,12 +1,14 @@
 loop = 1
 while loop == 1:
     print("___________________________________________")
-    sacar = float(input("Digite o montante a sacar..."))
+    sacar = input("Digite o montante a sacar...")
+    if (sacar.isdigit()):
+        saque = float(sacar)
     print("___________________________________________")
     metodo = int(input("Digite o metodo [1] Pix [2] Maquininha de Cartao ...."))
 
     if metodo == 1:
-        total = sacar + (sacar * 0.1)
+        total = saque + (saque * 0.1)
         print("Total a sacar: {}".format(total))
         continuar = input("Digite [Y] para continuar ou [N] para sair...")
         continuar = continuar.upper()
@@ -18,7 +20,7 @@ while loop == 1:
             input("Caractere invalido * {} * nao declarado no escopo...")
             continue
     elif metodo == 2:
-        total = sacar + (sacar * 0.3)
+        total = saque + (saque * 0.3)
         print("Total a sacar: {}".format(total))
         continuar = input("Digite [Y] para continuar ou [N] para sair...")
         continuar = continuar.upper()
@@ -31,7 +33,7 @@ while loop == 1:
             continue
     elif metodo == 3:
         taxa = int(input("Digite a taxa para sacar..."))
-        total = sacar + (sacar * (taxa / 100))
+        total = saque + (saque * (taxa / 100))
         print("Total a sacar: {}".format(total))
         continuar = input("Digite [Y] para continuar ou [N] para sair...")
         continuar = continuar.upper()
